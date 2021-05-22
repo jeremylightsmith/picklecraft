6f717b1c2f34595470fa1c366777fe7ab1f71dd6
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2021-05-22 02:09:05 -0700

    place a block from ruby!!!

--- src/main/java/com/picklepop/pickle/WebServer.java
+++ src/main/java/com/picklepop/pickle/WebServer.java
@@ -3,8 +3,13 @@ package com.picklepop.pickle;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +17,13 @@ import org.apache.logging.log4j.Logger;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+// import org.json.simple.*;
+import org.json.*;
 
 public class WebServer {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -37,42 +49,90 @@ public class WebServer {
         }
 
         public void handle(HttpExchange t) throws IOException {
-            String path = String.format("%s %s", t.getRequestMethod(), t.getRequestURI());
+            String response = "";
+            try {
+                String path = String.format("%s %s", t.getRequestMethod(), t.getRequestURI());
 
-            System.out.println("path = " + path);
-            LOGGER.info("patttth = " + path);
+                switch (path) {
+                    case "POST /blocks/new":
+                        response = placeBlock(parseParams(t.getRequestBody()));
+                        break;
 
-            String responseBody = "";
-
-            switch (path) {
-                case "POST /blocks/new":
-                    newBlock(readStream(t.getRequestBody()));
-                    break;
-
-                case "GET /players":
-                    responseBody = getPlayers();
-                    break;
+                    case "GET /players":
+                        response = getPlayers();
+                        break;
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+                renderText(t, e.getMessage(), 500);
+                return;
             }
+            renderText(t, response, 200);
+        }
 
-            byte[] responseBodyBytes = responseBody.getBytes();
-
-            t.sendResponseHeaders(200, responseBodyBytes.length);
+        private void renderText(HttpExchange t, String body, int status) throws IOException {
+            byte[] responseBodyBytes = body.getBytes();
+            t.sendResponseHeaders(status, responseBodyBytes.length);
             OutputStream os = t.getResponseBody();
             os.write(responseBodyBytes);
             os.close();
         }
 
-        private void newBlock(String stuff) {
-            System.out.println("new block: " + stuff);
+        private String placeBlock(Map params) {
+            System.out.println("placeMap(type: "+params.get("type")+", ("+params.get("x")+", "+params.get("y")+", "+params.get("z")+")");
+
+            Block block = getBlock((String) params.get("type"));
+            BlockPos pos = new BlockPos(
+                    ((Number) params.get("x")).intValue(),
+                    ((Number) params.get("y")).intValue(),
+                    ((Number) params.get("z")).intValue()
+            );
+
+            ServerWorld world = this.server.overworld();
+
+            world.setBlock(pos, block.defaultBlockState(), 3);
+
+            return "OK";
+        }
+
+        private Block getBlock(String type) {
+            switch(type) {
+                case "COBBLESTONE":
+                    return Blocks.COBBLESTONE;
+                case "IRON_ORE":
+                    return Blocks.IRON_ORE;
+                case "GOLD_ORE":
+                    return Blocks.GOLD_ORE;
+                case "NETHERITE_BLOCK":
+                    return Blocks.NETHERITE_BLOCK;
+                case "WATER":
+                    return Blocks.WATER;
+                case "ACACIA_WOOD":
+                    return Blocks.ACACIA_WOOD;
+                default:
+                    return Blocks.AIR;
+            }
         }
 
         private String getPlayers() {
             ServerWorld world = this.server.overworld();
             List<ServerPlayerEntity> players = world.players();
 
-            ServerPlayerEntity player = world.getRandomPlayer();
+            JSONArray jPlayers = new JSONArray();
+            for (ServerPlayerEntity player : players) {
+                JSONArray jPosition = new JSONArray();
+                jPosition.add(player.position().x);
+                jPosition.add(player.position().y);
+                jPosition.add(player.position().z);
 
-            return player.toString();
+                JSONObject jPlayer = new JSONObject();
+                jPlayer.put("id", player.getId());
+                jPlayer.put("name", player.getName().getString());
+                jPlayer.put("position", jPosition);
+                jPlayers.add(jPlayer);
+            }
+
+            return jPlayers.toString();
         }
 
         private String readStream(InputStream stream) throws IOException {
@@ -84,6 +144,14 @@ public class WebServer {
                 sb.append(str);
             }
             return sb.toString();
+        }
+
+        private Map parseParams(InputStream stream) throws IOException {
+            try {
+                return (JSONObject) new JSONParser().parse(readStream(stream));
+            } catch (ParseException e) {
+                throw new IOException(e);
+            }
         }
     }
 }
